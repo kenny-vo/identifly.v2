f5fed5dc90421c0f5aac6aa25c0583cc521dbdf6
Kenneth Vo <kennyvo91@gmail.com>
2017-07-25 14:28:47 -0500

    added controllers

---
+++ controllers/listingsController.js
@@ -0,0 +1,16 @@
+/************
+ * DATABASE *
+ ************/
+
+ var db = require('../models');
+
+ // GET /api/listings
+function index (req, res) {
+  db.Listing.find{}, function(err, allListings) {
+    res.json(allListings);
+  }
+}
+
+module.exports = {
+  index: index
+}
---
+++ public/scripts/app.js
@@ -0,0 +1,29 @@
+angular
+  .module('identifly', ['ngRoute'])
+  .config(config);
+
+config.$inject = ['$routeProvider', '$locationProvider'];
+
+function confid ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateURL: '/templates/listings',
+      controllerAs: 'listingsIndexCtrl',
+      controller: 'ListingsIndexController'
+    })
+    .when('/listings', {
+      templateURL: '/templates/listings',
+      controllerAs: 'listingsIndexCtrl',
+      controller: 'ListingsIndexController'
+    })
+    .when('/listings/:id', {
+      templateURL: '/templates/listings-show',
+      controllerAs: 'listingsShowCtrl',
+      controller: 'ListingsShowController'
+    })
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+}
