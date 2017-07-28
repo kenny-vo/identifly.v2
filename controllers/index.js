4dfe512e808a030fbdeee7ccfe639aa36117b330
Kenneth Vo <kennyvo91@gmail.com>
2017-07-27 22:33:00 -0500

    bower route

--- controllers/index.js
+++ controllers/index.js
@@ -2,4 +2,4 @@ module.exports.api = require('./apiController');
 module.exports.listings = require('./listingsController');
 
 var mongoose = require('mongoose');
-mongoose.connect( process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds127173.mlab.com:27173/heroku_mqrs4cp1");
+mongoose.connect( process.env.MONGODB_URI );
